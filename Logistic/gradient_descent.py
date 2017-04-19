0dbf831440a3b624d8d6809dfb5823e65ad73330
nilesh4145 <n.chaturvedi3@gmail.com>
2017-04-20 02:21:02 +0530

    Code

--- Logistic/gradient_descent.py
+++ Logistic/gradient_descent.py
@@ -15,7 +15,7 @@ descent_rate = float(input("Rate of descent : "))
 starting_point = float(input("Starting point : "))
 
 grad_coef_vector = []
-permessible_error = 0.0000001
+permissible_error = 0.0000001
 gradient = 0
 
 x0 = starting_point
@@ -25,27 +25,30 @@ power = int(degree)
 for x in range(power+1):
 	grad_coef_vector.append(power * int(coefs[x]))
 	power -= 1 
+
+grad_coef_vector = list(reversed(grad_coef_vector))
+
 print(grad_coef_vector)
 
 
-error = 999999999999999
-while (error >= permessible_error):
-	# Calculate gradient
-	power = int(degree)-1
-	
-	for i in grad_coef_vector:
-		if(power>0):
-			gradient += i * math.pow(x0, power)
-		else:
-			gradient += 0 
-		power-=1	
+error = float('inf')
+while (error >= permissible_error):
+	power = 0
+	gradient = 0
+	if(xn == 0):
+		gradient  = grad_coef_vector[1]
+	else:
+		for i in grad_coef_vector[1:]:
+			gradient += i * math.pow(xn, power) 
+			power+=1
 
+	# Calculate gradient	
 	print("gradient %d"%gradient)
 	xn = x0 - (descent_rate * gradient)
 	print("xn %d"%xn)
 	error = abs(xn - x0)
 	print("error %d"%error)
-	xn = 0
 	x0=xn
+
 
 print(" Point of minima is : {}".format(xn))
