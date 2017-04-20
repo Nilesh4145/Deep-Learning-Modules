71dae6e108ed96a439ade8d847d5c24a1035a066
nilesh4145 <n.chaturvedi3@gmail.com>
2017-04-20 09:59:36 +0530

    Update

--- Cost Visualization/visual.py
+++ Cost Visualization/visual.py
@@ -13,10 +13,6 @@ def linear(x, y, X, Y):
 	bias = 5
 	return x*X + y*Y + bias
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')
-ax2 = fig.add_subplot(121, projection='3d')
-ax3 = fig.add_subplot(222, projection='3d')
 n = 10
 xs = np.linspace(0, 1, 50)
 ys = np.linspace(0, 1, 50)
@@ -24,21 +20,31 @@ z1 = [logit(x, y, X1[0], X1[1]) for x,y in zip(xs,ys)]
 z2 = [logit(x, y, X2[0], X2[1]) for x,y in zip(xs,ys)]
 z3 = [linear(x, y, X2[0], X2[1]) for x,y in zip(xs,ys)]
 
-ax1.plot(xs, ys, z1, label = "Logistic Regression with dataset 1")
-ax2.plot(xs, ys, z2, label = "Logistic Regression with dataset 2")
-ax3.plot(xs, ys, z3, label = "Linear Regression with dataset 2")
+plt.figure()
 
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
-ax1.set_zlabel('Z')
+plt.subplot(221, projection='3d')
+plt.plot(xs, ys, z1, label = "Logistic Regression with dataset 1")
+# plt.xlabel('X')
+# plt.ylabel('Y')
+# plt.zlabel('Z')
+plt.title('Logistic Regression with dataset 1')
+plt.grid(True)
 
-ax2.set_xlabel('X')
-ax2.set_ylabel('Y')
-ax2.set_zlabel('Z')
+plt.subplot(222, projection='3d')
+plt.plot(xs, ys, z2, label = "Logistic Regression with dataset 2")
+# plt.xlabel('X')
+# plt.ylabel('Y')
+# plt.zlabel('Z')
+plt.title('Logistic Regression with dataset 2')
+plt.grid(True)
 
-ax3.set_xlabel('X')
-ax3.set_ylabel('Y')
-ax3.set_zlabel('Z')
+plt.subplot(223, projection='3d')
+plt.plot(xs, ys, z3, label = "Linear Regression with dataset 2")
+# plt.xlabel('X')
+# plt.ylabel('Y')
+# plt.zlabel('Z')
+plt.title('Linear Regression with dataset 2')
+plt.grid(True)
 
 
 
